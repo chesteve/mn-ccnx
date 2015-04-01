737bf59c085e8b6f23327f4458e29ab5d4309bc6
Caio Elias <caiom.elias@gmail.com>
2015-04-01 15:46:46 -0300

    first steps into integrating switches with the tool

--- mininet/conf_parser.py
+++ mininet/conf_parser.py
@@ -8,10 +8,41 @@ class confCCNHost():
         self.uri_tuples = uri_tuples
         self.cpu = cpu
         self.cores = cores
-	self.cache = cache
+        self.cache = cache
 
     def __repr__(self):
         return 'Name: ' + self.name + ' App: ' + self.app + ' URIS: ' + str(self.uri_tuples) + ' CPU:' + str(self.cpu) + ' Cores:' +str(self.cores) + ' Cache: ' + str(self.cache)
+
+class confSwitch():
+
+    def __init__(self, name, switchType='', stopCommand='', sflow=None, switchIP='', dpid=None, dpctl='', startCommand='', netflow=None, externalInterfaces='', controllers=''):
+        self.name = name
+        self.switchType = switchType
+        self.stopCommand = stopCommand
+        self.sflow = sflow
+        self.switchIP = switchIP
+        self.dpid = dpid
+        self.dpctl = dpctl
+        self.startCommand = startCommand
+        self.netflow = netflow
+        self.externalInterfaces = externalInterfaces
+        self.controllers = controllers
+
+    def __repr__(self):
+        return 'Name: ' + self.name + ' SwitchType: ' + self.switchType + ' StopCommand: ' + self.stopCommand + ' sflow:' + str(self.sflow) + ' SwitchIP:' + self.switchIP + ' dpid: ' + str(self.dpid) + ' dpctl: ' + self.dpctl + ' StartCommand: ' + self.startCommand + ' Netflow: ' + str(self.netflow) + ' ExternalInterfaces: ' + ','.join(self.externalInterfaces) + ' Controllers: ' + ','.join(self.controllers)
+
+class confController():
+
+    def __init__(self, name, remotePort=None, controllerProtocol='', remoteIP='', controllerType=''):
+        self.name = name
+        self.remotePort = remotePort
+        self.controllerProtocol = controllerProtocol
+        self.remoteIP = remoteIP
+        self.controllerType = controllerType
+
+    def __repr__(self):
+        return 'Name: ' + self.name + ' RemotePort: ' + str(self.remotePort) + ' controllerProtocol: ' + self.controllerProtocol + ' RemoteIP:' + self.remoteIP + ' ControllerType:' + self.controllerType
+
 
 class confCCNLink():
 
@@ -34,10 +65,10 @@ def parse_hosts(conf_arq):
 	
 	#makes a first-pass read to hosts section to find empty host sections
     for item in items:
-	name = item[0]
-	rest = item[1].split()
-	if len(rest) == 0:
-		config.set('hosts', name, '_')
+	    name = item[0]
+	    rest = item[1].split()
+	    if len(rest) == 0:
+		    config.set('hosts', name, '_')
 	#updates 'items' list
     items = config.items('hosts')
 
@@ -54,21 +85,21 @@ def parse_hosts(conf_arq):
         uri_list=[]
         cpu = None
         cores = None
-	cache = None
+        cache = None
 
         for uri in uris:
             if re.match("cpu",uri):
                 cpu = float(uri.split('=')[1])
             elif re.match("cores",uri):
                 cores = uri.split('=')[1]
-	    elif re.match("cache",uri):
-		cache = uri.split('=')[1]
-	    elif re.match("mem",uri):
-		mem = uri.split('=')[1]	   	
+            elif re.match("cache",uri):
+                cache = uri.split('=')[1]
+            elif re.match("mem",uri):
+                mem = uri.split('=')[1]	   	
             else:
                 uri_list.append((uri.split(',')[0],uri.split(',')[1]))
 
-        hosts.append(confCCNHost(name , app, uri_list,cpu,cores,cache))
+        hosts.append(confCCNHost(name, app, uri_list, cpu, cores, cache))
 
     return hosts
 
@@ -86,28 +117,28 @@ def parse_routers(conf_arq):
 
         rest = item[1].split()
 
-        uris = rest
         uri_list=[]
+
         cpu = None
         cores = None
-	cache = None
+        cache = None
 
-	if '_' in uris:
-		pass
-	else:
-		for uri in uris:
-		    if re.match("cpu",uri):
-			cpu = float(uri.split('=')[1])
-		    elif re.match("cores",uri):
-			cores = uri.split('=')[1]
-		    elif re.match("cache",uri):
-			cache = uri.split('=')[1]
-		    elif re.match("mem",uri):
-			mem = uri.split('=')[1]
-		    else:
-			uri_list.append((uri.split(',')[0],uri.split(',')[1]))
+        if '_' in rest:
+            pass
+        else:
+            for opt in rest:
+		        if re.match("cpu",opt):
+			        cpu = float(opt.split('=')[1])
+		        elif re.match("cores",opt):
+			        cores = opt.split('=')[1]
+		        elif re.match("cache",opt):
+			        cache = opt.split('=')[1]
+		        elif re.match("mem",opt):
+			        mem = opt.split('=')[1]
+		        else:
+			        uri_list.append((opt.split(',')[0],opt.split(',')[1]))
 
-        routers.append(confCCNHost(name=name , uri_tuples=uri_list, cpu=cpu, cores=cores))
+        routers.append(confCCNHost(name=name, uri_tuples=uri_list, cpu=cpu, cores=cores, cache=cache))
 
     return routers
 
@@ -129,9 +160,9 @@ def parse_links(conf_arq):
 
         args = line.split()
 	
-	#checks for non-empty line
-	if len(args) == 0:
-		continue
+	    #checks for non-empty line
+        if len(args) == 0:
+            continue
 
         h1, h2 = args.pop(0).split(':')
 
@@ -149,5 +180,99 @@ def parse_links(conf_arq):
 
         links.append(confCCNLink(h1,h2,link_dict))
 
-
     return links
+
+def parse_switches(conf_arq):
+    'Parse switches section from the conf file.'
+    config = ConfigParser.RawConfigParser()
+    config.read(conf_arq)
+
+    def parse_list(inString):
+        outList=[]
+
+        for item in inString.split(','):
+            outList.append(item)
+
+        return outList 
+
+    switches = []
+
+    items = config.items('switches')
+
+    for item in items:
+        name = item[0]
+
+        rest = item[1].split()
+
+        switchType = ''
+        stopCommand = ''
+        sflow = None
+        switchIP = ''
+        dpid = None
+        dpctl = ''
+        startCommand = ''
+        netflow = None
+        externalInterfaces = ''
+        controllers = ''
+
+        for opt in rest:
+            if re.match("switchType",opt):
+                switchType = opt.split('=')[1]
+                if (switchType == 'legacySwitch'):
+                    break
+            elif re.match("stopCommand",opt):
+			    stopCommand = opt.split('=')[1]
+            elif re.match("sflow",opt):
+			    sflow = int(opt.split('=')[1])
+            elif re.match("switchIP",opt):
+			    switchIP = opt.split('=')[1]
+            elif re.match("dpid",opt):
+			    dpid = int(opt.split('=')[1])
+            elif re.match("dpctl",opt):
+			    dpctl = opt.split('=')[1]
+            elif re.match("startCommand",opt):
+			    startCommand = opt.split('=')[1]
+            elif re.match("netflow",opt):
+			    netflow = int(opt.split('=')[1])
+            elif re.match("externalInterfaces",opt):
+			    externalInterfaces = parse_list(opt.split('=')[1])
+            elif re.match("controllers",opt):
+			    controllers = parse_list(opt.split('=')[1])
+
+        switches.append(confSwitch(name, switchType, stopCommand, sflow, switchIP, dpid, dpctl, startCommand, netflow, externalInterfaces, controllers))
+
+    return switches
+
+def parse_controllers(conf_arq):
+    'Parse controllers section from the conf file.'
+    config = ConfigParser.RawConfigParser()
+    config.read(conf_arq)
+
+    controllers = []
+
+    items = config.items('controllers')
+
+    for item in items:
+        name = item[0]
+
+        rest = item[1].split()
+
+        remotePort = None
+        controllerProtocol = ''
+        remoteIP = ''
+        controllerType = ''
+
+        for opt in rest:
+            if re.match("remotePort",opt):
+                remotePort = int(opt.split('=')[1])
+            elif re.match("controllerProtocol",opt):
+                controllerProtocol = opt.split('=')[1]
+            elif re.match("remoteIP",opt):
+                remoteIP = opt.split('=')[1]
+            elif re.match("controllerType",opt):
+                controllerType = opt.split('=')[1]
+
+        controllers.append(confController(name, remotePort, controllerProtocol, remoteIP, controllerType))
+
+    return controllers
+
